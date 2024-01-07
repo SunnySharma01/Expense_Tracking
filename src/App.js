909d877941946531556b12e6222f9107887dfcd3
SunnySharma01 <sunny.kumar.che21@heritageit.edu.in>
2024-01-07 23:33:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Subcomponents from './Components/Subcomponents';
+
+function App (){
+  return(
+    <div className='app-container'>
+    <h1>Let's get started </h1>
+    
+    <Subcomponents></Subcomponents> 
+    </div>
+    
+  );
+}
+export default App;
---
+++ src/Components/Subcomponents.js
@@ -0,0 +1,13 @@
+import './Subcomponents.css'
+function Subcomponents (){
+    return(
+        <div className='expense-item'>
+            <div className='color'>March 12th 2021</div>
+            <div className='expense-item_desription'> 
+            <h2>Car Insurence</h2>
+            <div className='expence-item_price'>$200</div>
+            </div>
+         </div>
+    );
+}
+export default Subcomponents;
